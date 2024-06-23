15aa857b502ae87525837b1970e699d0b0ec664e
a.pankratova <a.pankratova@digitalms.ru>
2024-06-23 17:35:52 +0300

    refactor(logo): уберет лишнюю обертку

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -7,10 +7,8 @@ export default {
 <template>
   <div class="logo">
     <img src="@/assets/images/symbol-t.png" alt="" class="image_symbol" />
-    <span class="wrapper">
-      <img src="@/assets/images/play.png" alt="" class="image" />
-      <img src="@/assets/images/stop.png" alt="" class="image" />
-    </span>
+    <img src="@/assets/images/play.png" alt="" class="image" />
+    <img src="@/assets/images/stop.png" alt="" class="image" />
     <img src="@/assets/images/symbol-p.png" alt="" class="image_symbol" />
   </div>
 </template>
@@ -20,21 +18,23 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 10px;
+  gap: 3px;
 }
 
 .image_symbol {
   width: 44px;
 }
 
+.image_symbol:first-child {
+  margin-right: 7px;
+}
+
+.image_symbol:last-child {
+  margin-left: 7px;
+}
+
 .image {
   width: 16px;
   height: 24px;
-}
-
-.wrapper {
-  display: flex;
-  align-items: center;
-  gap: 3px;
 }
 </style>
